54ccdfaa7eb52362acb40eaa6c4dd4ffb61d09fb
frieda666 <906691204@qq.com>
2023-08-24 22:36:21 -0700

    prettier

--- service/flaskr/__init__.py
+++ service/flaskr/__init__.py
@@ -1,24 +1,20 @@
 import os
 import glob
-
 import matplotlib
-matplotlib.use('Agg') #main threading issues with matplotlib
 import numpy as np
 import xarray
 from flask import Flask, request, send_from_directory
 from flask_cors import cross_origin
 import matplotlib.image
 import matplotlib.pyplot as plt
-from werkzeug.serving import run_simple
 from .county import query_county
-
-from PIL import Image
-
 import geopandas
 from shapely.geometry import mapping
-from rasterio.mask import mask
 
+# main threading issues with matplotlib
+matplotlib.use('Agg')
 cali_shape = geopandas.read_file("./california_shp/CA_State_TIGER2016.shp")
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -26,7 +22,6 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
     if test_config is None:
@@ -39,7 +34,8 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    #Disable caching for image and legend. Currently works on the Google Chrome and Microsoft Edge browsers without the need to checkmark disable cache option.
+    # Disable caching for image and legend. Currently works on the Google Chrome and Microsoft Edge browsers
+    # without the need to checkmark disable cache option.
     @app.after_request
     def add_header(response):
         response.headers["Cache-Control"]  = "no-store max-age=0"
@@ -49,11 +45,9 @@ def create_app(test_config=None):
     @cross_origin()
     def make_query():
         cleanup()
-
         start_date, end_date = request.args.get('start_date'), request.args.get('end_date')
         if start_date is not None and end_date is not None:
             return query(int(start_date), int(end_date))
-
         return 'failed'
 
     @app.route('/image', methods=['GET'])
@@ -74,6 +68,7 @@ def create_app(test_config=None):
 
     return app
 
+
 def cleanup():
     files_to_clean = glob.glob(f'{os.getcwd()}/burn-window-*.nc')
     for f in files_to_clean:
@@ -82,6 +77,7 @@ def cleanup():
         except:
             print("Error while deleting file : ", f)
 
+
 def query(start_date: int, end_date: int):
     print("Querying against netcdf.")
     with xarray.open_dataset("window.nc") as burn_windows_dataset:
@@ -89,18 +85,18 @@ def query(start_date: int, end_date: int):
         flattened_window = xarray.DataArray(coords=[burn_windows.coords['lat'][:], burn_windows.coords['lon'][:]],
                                             dims=['lat', 'lon'])
 
-        flattened_window.data = np.sum(burn_windows.data[start_date:end_date + 1, :, :], axis=0) # Sum data between a period of time (in days)
+        # Sum data between a period of time (in days)
+        flattened_window.data = np.sum(burn_windows.data[start_date:end_date + 1, :, :], axis=0)
         
-        #Clip data to the outline of California using shapefile
+        # Clip data to the outline of California using shapefile
         flattened_window = flattened_window.rio.set_spatial_dims(x_dim='lon', y_dim='lat')
         flattened_window.rio.write_crs("EPSG:4326", inplace=True)
-        print("HERE")
         area_in_window = flattened_window.rio.clip(cali_shape.geometry.apply(mapping), cali_shape.crs, drop=True)
 
-        #Create duplicate and clip again 
+        # Create duplicate and clip again
         duplicate = xarray.DataArray(
             data=area_in_window.where(area_in_window.notnull(), np.nan),
-            coords=area_in_window.coords,  #Use the same coordinates as area_in_window
+            coords=area_in_window.coords,  # Use the same coordinates as area_in_window
             dims=["lat", "lon"]
         )
 
@@ -108,7 +104,7 @@ def query(start_date: int, end_date: int):
         duplicate.rio.write_crs("EPSG:4326", inplace=True)
         duplicate_clipped = duplicate.rio.clip(cali_shape.geometry.apply(mapping), cali_shape.crs, drop=True)
 
-        #Create Legend and Burn-Window Map
+        # Create Legend and Burn-Window Map
         fig, ax = plt.subplots()
         fig.patch.set_visible(False)
         ax.axis('off')
@@ -121,9 +117,8 @@ def query(start_date: int, end_date: int):
 
         plt.colorbar(ax = ax, label="Day", boundaries=np.linspace(0, number_of_total_days_in_burn_window))
         ax.remove()
-        #plt.show()
         plt.close(fig)
-        fig.savefig('legend.png', bbox_inches = 'tight', pad_inches = 0, dpi = 1200)
+        fig.savefig('legend.png', bbox_inches='tight', pad_inches=0, dpi=1200)
         fig.savefig('legend.svg', format='svg', dpi=1500)
         allow_svg_to_stretch('legend.svg')
 
